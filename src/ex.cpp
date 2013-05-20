97bb46833851b4b97f67c27a30bd45a34fd0714c
Laurent <laurent.gatto@gmail.com>
2013-05-20 11:25:03 +0100

    exercises in R

--- src/ex.cpp
+++ src/ex.cpp
@@ -91,6 +91,9 @@ NumericVector rollP3(NumericVector A, int start, int end) {
   return B.sort() ;
 }
 
+
+// from Rcpp galery, Hadley Wickham
+
 // [[Rcpp::export]]
 List scalar_missings() {
   int int_s = NA_INTEGER;
@@ -119,6 +122,9 @@ NumericVector biggerYsubset(NumericVector x, double y) {
   }
   return ans;
 }
+
+
+// Example from Dirk's slides
 
 
 // foo <- function(x, y) ifelse(x < y, x*x, -(y*y))
@@ -170,41 +176,6 @@ int fC(int n) {
 }
 
 // benchmark(fr(20), fC(20))[, 1:4]
-
-
-
-// from http://gallery.rcpp.org/articles/vector-cumulative-sum/
-
-#include <Rcpp.h>
-#include <numeric> // for std::partial_sum
-
-using namespace Rcpp;
-
-// [[Rcpp::export]]
-NumericVector cumsum1(NumericVector x){
-  // initialize an accumulator variable
-  double acc = 0;
-  // initialize the result vector
-  NumericVector res(x.size());
-  for(int i = 0; i < x.size(); i++){
-    acc += x[i];
-    res[i] = acc;
-  }
-  return res;
-}
-
-// [[Rcpp::export]]
-NumericVector cumsum2(NumericVector x) {
-  NumericVector res(x.size());
-  std::partial_sum(x.begin(), x.end(),
-		   res.begin());
-  return res;
-}
-
-// [[Rcpp::export]]
-NumericVector cumsum3(NumericVector x){
-  return cumsum(x); // compute + return result vector
-}
 
 
 // [[Rcpp::export]]
