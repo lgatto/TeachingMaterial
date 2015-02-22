78a78fe62b5b91f29a8ad541daa078b1c5b17073
brianhigh <bhigh@live.com>
2015-02-21 23:03:23 -0800

    Cleaned up hg19.fa section and comments.

--- using_rsem_prep_input.sh
+++ using_rsem_prep_input.sh
@@ -4,26 +4,31 @@
 # Or you can download them (hg19.fa, UCSC.gtf, and knownIsoforms) from:
 # https://www.dropbox.com/sh/p4cosmsqwtmpce7/AADgXoeK4RwBTfDlDUr_dwQYa?dl=0
 # We do this in the "Prepare reference genome" section of Using_RSEM.Rmd.
-
-# Required utilities: 
-# bash which basename curl tar cat touch grep chmod ls rm gunzip genePredToGtf
 #
-# - genePredToGtf is from kentUtils: https://github.com/ENCODE-DCC/kentUtils
-# - The rest will be standard on a Unix, Linux, or Mac OSX system.
-# - This script has not been tested on a Windows/Cygwin system. 
-
 # Parts of this script were adapted from:
 #     http://watson.nci.nih.gov/~sdavis/tutorials/biowulf-2011/
 #     http://genomewiki.ucsc.edu/index.php/Genes_in_gtf_or_gff_format
 
+# Required utilities: 
+# - bash which basename curl tar cat touch grep chmod gunzip genePredToGtf
+#
+# Notes:
+# - genePredToGtf is from kentUtils: https://github.com/ENCODE-DCC/kentUtils
+# - The rest will be standard on a Unix, Linux, or Mac OSX system.
+# - This script has not been tested on a Windows/Cygwin system. 
+
 # Steps performed by this script are:
-# 1. Download the human genome hg19 chromosome "fa" files (chromFa.tar.gz)
-# 2. Combine the individual "fa" files into a single "hg19.fa" file
-# 3. Download known isoforms file
-# 4. Create GTF file from known gene file
+# 1. Download and combine the human genome hg19 chromosome "fa" files
+# 2. Download known isoforms file
+# 3. Download known gene file and convert to GTF file
+
+# After successfully running this script, you should be able to run:
+#    rsem-prepare-reference --gtf UCSC.gtf \
+#        --transcript-to-gene-map knownIsoforms \ 
+#        --bowtie2 hg19.fa hg19
 
 # Make sure we have the tools we need
-for i in curl tar touch grep chmod ls cat rm gunzip genePredToGtf; do \
+for i in curl tar touch grep chmod cat gunzip genePredToGtf; do \
     which "$i" >/dev/null
     if [ $? -ne 0 ]; then \
         echo "Can't find $i. Aborting!" && exit 1
@@ -36,19 +41,14 @@ HG19PATH='http://hgdownload.cse.ucsc.edu/goldenPath/hg19'
 # Download, extract and combine chromosome files (unless already done)
 CHROMEPATH='bigZips/chromFa.tar.gz'
 URL="$HG19PATH/$CHROMEPATH"
-CHROMEFILE=$(basename "$URL")
 BIGFA="hg19.fa"
-if [ ! -s "$BIGFA" ]; then \
-    [ -s "$CHROMEFILE" ] || curl -O "$URL"
-    ls chr*.fa 2>/dev/null 1>/dev/null || tar xvzf "$CHROMEFILE"
-    [ $? -eq 0 ] && cat "chr*.fa" >> "$BIGFA" && rm -f chr*.fa
-fi
+[ -s "$BIGFA" ] || curl -s -o - "$URL" | tar xzf - -O > "$BIGFA"
 
 # Download and extract known isoforms file (unless already done)
 ISOFORMSPATH='database/knownIsoforms.txt.gz'
 URL="$HG19PATH/$ISOFORMSPATH"
 ISOFORMS=$(basename "$URL" .txt.gz)
-[ -s "$ISOFORMS" ] || (curl "$URL" | gunzip -c - > "$ISOFORMS")
+[ -s "$ISOFORMS" ] || (curl -s "$URL" | gunzip -c - > "$ISOFORMS")
 
 # Download known gene file and create GTF file
 GTF='UCSC.gtf'
@@ -61,17 +61,3 @@ db.user=genomep
 db.password=password
 central.db=hgcentral' >> "$HGCONF"
 [ -s "$GTF" ] || genePredToGtf hg19 knownGene "$GTF"
-#
-# Alternatively, you can follow the steps from the URL below to get the GTF:
-# https://groups.google.com/a/soe.ucsc.edu/d/msg/genome/kyk7AAm4R-M/9LkE-CRjzioJ
-# However, you should note the following quote from the RSEM README: 
-#     https://github.com/bli25wisc/RSEM#-usage 
-# "Please note that GTF files generated from the UCSC Table Browser do not 
-# contain isoform-gene relationship information. However, if you use the UCSC 
-# Genes annotation track, this information can be recovered by downloading the 
-# knownIsoforms.txt file for the appropriate genome."
-
-# Now you should be ready to run rsem-prepare-reference on these files:
-#     rsem-prepare-reference --gtf UCSC.gtf \
-#         --transcript-to-gene-map knownIsoforms \ 
-#         --bowtie2 hg19.fa hg19
