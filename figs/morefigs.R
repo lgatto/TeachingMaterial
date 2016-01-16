a28dcfb52d476a49426fff58f89d6cc3350b3cfc
Laurent <lg390@cam.ac.uk>
2016-01-16 23:56:48 +0000

    more updates

--- figs/morefigs.R
+++ figs/morefigs.R
@@ -1,5 +1,7 @@
 pdf("moredesigns.pdf", width = 10, height = 9)
 
+library("ggplot2")
+
 xs <- c(1:4, 6:9, 11:14, 16:19)
 cex <- 6
 
@@ -44,4 +46,34 @@ x <- data.frame(strain, growth = mns, food, facet)
 ggplot(aes(x = strain, y = growth, colour = food), data = x) +
     geom_point() +
     facet_grid(~facet)
+dev.off()
+
+
+pdf("power.pdf", width = 10, height = 7)
+deltas <- c(2, 2, 2, 1, 2)
+sds <- c(1, 1, 2, 1.5, 4)
+sig <- c(0.01, 0.05, 0.05, 0.05, 0.05)
+pw <- c(seq(0.5, 0.95, 0.05), 0.99)
+res <- matrix(NA, 11, length(deltas))
+rownames(res) <- pw
+for (i in 1:length(deltas)) {
+    res[, i] <- sapply(pw,
+                       function(p) power.t.test(delta = deltas[i],
+                                                sd = sds[i],
+                                                sig.level = sig[i],
+                                                power = p)$n)
+}
+matplot(res, type = "b",
+        col = 1:5,
+        lty = 1,
+        xaxt = "n",
+        xlab = "Power",
+        ylab = "Sample size (per group)")
+axis(side = 1, at = 1:length(pw), labels = pw)
+legend("topleft",
+       paste0("Delta: ", deltas, " sd: ", sds, " signif: ", sig),
+       lty = 1, bty = "n",
+       col = 1:6)
+grid()
+abline(h = 3)
 dev.off()
