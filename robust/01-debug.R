36ae8c551077c68c57b198745cc66f40785f15ff
Laurent <lg390@cam.ac.uk>
2017-04-02 17:27:06 +0100

    add col_means defensive exercise

--- robust/01-debug.R
+++ robust/01-debug.R
@@ -80,6 +80,26 @@ is.string("letters")
 is.string(letters)
 
 
+col_means <- function(df) {
+  numeric <- sapply(df, is.numeric)
+  numeric_cols <- df[, numeric]
+  data.frame(lapply(numeric_cols, mean))
+}
+
+
+col_means(mtcars)
+col_means(mtcars[, 0])
+col_means(mtcars[0, ])
+col_means(mtcars[, "mpg", drop = FALSE])
+col_means(1:10)
+col_means(as.matrix(mtcars))
+col_means(as.list(mtcars))
+
+mtcars2 <- mtcars
+mtcars2[-1] <- lapply(mtcars2[-1], as.character)
+col_means(mtcars2)
+
+
 e <- function(i) {
   x <- 1:4
   if (i < 5) x[1:2]
